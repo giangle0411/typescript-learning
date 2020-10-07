0f9ce422f751bb19ecb2597ba54842592d7287ce
Giang Le <giangle0411@gmail.com>
2020-10-07 19:14:33 +1100

    map implement

--- maps/src/User.ts
+++ maps/src/User.ts
@@ -1,7 +1,23 @@
-class User {
+// Import Type definition file
+import faker from 'faker'
+
+// Typescript doesn't usually use export default
+//  Avoid export default while we can (community convention)
+export class User {
   name: string
   location: {
     lat: number
     lng: number
+  }
+  constructor() {
+    this.name = faker.name.firstName()
+    this.location = {
+      lat: parseFloat(faker.address.latitude()),
+      lng: parseFloat(faker.address.longitude()),
+    }
+  }
+
+  markerContent(): string {
+    return `User Name: ${this.name}`
   }
 }
--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,1 +1,11 @@
-console.log('Hi there!')
+import { User } from './User'
+import { Company } from './Company'
+import { CustomMap } from './CustomMap'
+
+const company = new Company()
+const user = new User()
+// create a map
+const customMap = new CustomMap('map')
+
+customMap.addMarker(user)
+customMap.addMarker(company)
