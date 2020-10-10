65c9e734eb44cd2858f815995d29b5f2870c1874
Giang Le <giangle0411@gmail.com>
2020-10-10 21:15:00 +1100

    improve csv data reading

---
+++ stats/build/MatchResult.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.MatchResult = void 0;
+// enum - enumeration
+// this is a collection of closely related values
+// also a type
+var MatchResult;
+(function (MatchResult) {
+    MatchResult["HomeWin"] = "H";
+    MatchResult["AwayWin"] = "A";
+    MatchResult["Draw"] = "D";
+})(MatchResult = exports.MatchResult || (exports.MatchResult = {}));
--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,12 +1,18 @@
-import fs from 'fs'
+import { CsvFileReader } from './CsvFileReader'
+import { MatchResult } from './MatchResult'
 
-const matches = fs
-  .readFileSync('football.csv', {
-    encoding: 'utf-8',
-  })
-  .split('\n')
-  .map((row: string): string[] => {
-    return row.split(',')
-  })
+// 1. Read the csv files
+const reader = new CsvFileReader('football.csv')
+reader.read()
 
-console.log(matches)
+let chelseaWins = 0
+
+for (let match of reader.data) {
+  if (match[1] === 'Chelsea' && match[5] === MatchResult.HomeWin) {
+    chelseaWins++
+  } else if (match[2] === 'Chelsea' && match[5] === MatchResult.AwayWin) {
+    chelseaWins++
+  }
+}
+
+console.log(`Chelsea won ${chelseaWins} games`)
