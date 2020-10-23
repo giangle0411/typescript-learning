a1e0000874b351b86c3b76626a72fca63a4c526f
Giang Le <giangle0411@gmail.com>
2020-10-23 23:33:05 +1100

    todo actions, reducers, fetchTodos

--- react-redux-typescript/src/components/App.tsx
+++ react-redux-typescript/src/components/App.tsx
@@ -1,11 +1,36 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { Todo, fetchTodos } from '../actions'
+import { StoreState } from '../reducers'
 
-export class App extends Component {
+interface AppProps {
+  todos: Todo[]
+  fetchTodos(): any
+}
+
+class _App extends Component<AppProps> {
+  onButtonClick = (): void => {
+    this.props.fetchTodos()
+  }
+
+  renderList(): JSX.Element[] {
+    return this.props.todos.map((todo: Todo) => {
+      return <div key={todo.id}>{todo.title}</div>
+    })
+  }
+
   render() {
     return (
       <div>
-        <h3>Hi there!</h3>
+        <button onClick={this.onButtonClick}>Fetch</button>
+        {this.renderList()}
       </div>
     )
   }
 }
+
+const mapStateToProps = (state: StoreState): { todos: Todo[] } => {
+  return { todos: state.todos }
+}
+
+export const App = connect(mapStateToProps, { fetchTodos })(_App)
